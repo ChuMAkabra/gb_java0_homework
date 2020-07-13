0815f9ec5db050d776c80d00d3e795bae0d639f8
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-07-13 21:47:37 +0300

    Employee class and its implementation in Main class (currently empty)

---
+++ src/Assignment_05/Employee.java
@@ -0,0 +1,12 @@
+package Assignment_05;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+
+}
