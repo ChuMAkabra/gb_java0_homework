981d6f2904ac3ec61824f27123d38c23139abdec
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-07-13 21:47:51 +0300

    Employee class and its implementation in Main class (currently empty)

--- src/Assignment_05/Employee.java
+++ src/Assignment_05/Employee.java
@@ -4,9 +4,52 @@ public class Employee {
     private String fullName;
     private String position;
     private String email;
-    private int phoneNumber;
+    private int phone;
     private int salary;
     private int age;
 
+    public Employee() {
+    }
 
+    public Employee(String fullName) {
+        this();
+        this.fullName = fullName;
+    }
+
+    public Employee(String fullName, String position) {
+        this(fullName);
+        this.position = position;
+    }
+
+    public Employee(String fullName, String position, String email) {
+        this(fullName, position);
+        this.email = email;
+    }
+
+    public Employee(String fullName, String position, String email, int phone) {
+        this(fullName, position, email);
+        this.phone = phone;
+    }
+
+    public Employee(String fullName, String position, String email, int phone, int salary) {
+        this(fullName, position, email, phone);
+        this.salary = salary;
+    }
+
+    public Employee(String fullName, String position, String email, int phone, int salary, int age){
+        this(fullName, position, email, phone, salary);
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
 }
