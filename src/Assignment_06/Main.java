c5da1df5b2b1dafbd93c19ec88598176f83e217f
Dmitry Chumanov <dmitry.chumanov@gmail.com>
2020-07-20 22:20:23 +0300

    static field animals, and createAnimals() method added. Dog.swim() is used inside runJumpSwim. Instance check and type casting used for Dogs.

--- src/Assignment_06/Main.java
+++ src/Assignment_06/Main.java
@@ -1,17 +1,27 @@
 package Assignment_06;
 
+/**
+ * Что лучше?
+ * 1) Статическое поле animals и createAnimals(); runJumpSwim()?
+ * 2) локальная переменная animals и Animal[] animals= createAnimals(); runJumpSwim(animals)?
+ */
 public class Main {
+    private static Animal[] animals;
 
     public static void main(String[] args) {
+        createAnimals();
         runJumpSwim();
     }
 
-    public static void runJumpSwim() {
+    public static void createAnimals() {
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
         Cat cat1 = new Cat();
         Cat cat2 = new Cat();
-        Animal[] animals = new Animal[] {dog1, dog2, cat1, cat2};
+        animals = new Animal[] {dog1, dog2, cat1, cat2};
+    }
+
+    public static void runJumpSwim() {
 
         for (Animal animal: animals) {
             System.out.printf("%s с лимитом в %.2fм\n", animal.getSpecies(), animal.getRunLimit());
@@ -20,15 +30,15 @@ public class Main {
             System.out.printf("%s с лимитом в %.2fм\n", animal.getSpecies(), animal.getJumpLimit());
             animal.jump(0.5F);
             animal.jump(2.01F);
-        }
 
-        // поскольку плавать умеют только собаки, выполним проверку отдельно
-
-        Dog[] dogs = new Dog[] {dog1, dog2};
-        for (Dog dog : dogs) {
-            System.out.printf("%s с лимитом в %.2fм\n", dog.getSpecies(), dog.getSwimLimit());
-            dog.swim(10);
-            dog.swim(11);
+            // поскольку плавать умеют только собаки, выполним проверку
+            if (animal instanceof Dog) {
+                // решил попробовать вот такие приведения типов на практике
+                System.out.printf("%s с лимитом в %.2fм\n",
+                        animal.getSpecies(), ((Dog) animal).getSwimLimit());
+                ((Dog)animal).swim(10);
+                ((Dog)animal).swim(11);
+            }
         }
     }
 }
